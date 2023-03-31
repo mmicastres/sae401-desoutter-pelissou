218959fb1de1718e5bc67f3cd69ef7c0ef4d23dd
Jules-Pelissou <jules.pelissou@gmail.com>
2023-03-31 17:50:56 +0200

    Ajout de nouvelles routes

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,8 @@ Route::get('anecdote?valide=0', [App\Http\Controllers\AnecdotesController::class
 
 // Categories
 Route::get('categories', [App\Http\Controllers\CategoriesController::class, 'listeCategories']);
-
+Route::post('categories', [App\Http\Controllers\CategoriesController::class,'ajoutCategories']);
+Route::post('artiste/{pseudo}/titres/{id_titres/anecdotes', [App\Http\Controllers\AnecdotesController::class, 'ajoutAnecdote'])
 
 // A trier
 
